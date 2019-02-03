ec8018039377d0c6bbbee2589ffe085b73eff9fe
Lloyd Brookes <lloyd.brookes@gmail.com>
2019-02-03 11:40:55 +0000

    test

--- test.js
+++ test.js
@@ -6,8 +6,35 @@ const runner = new TestRunner()
 
 runner.test('simple', function () {
   const result = deepMerge({ a: 1 }, { b: 2 })
-  a.deepEqual({
+  a.deepEqual(result, {
     a: 1,
     b: 2
+  })
+})
+
+runner.test('one level', function () {
+  const result = deepMerge(
+    { a: 1, c: { d: 'd' } },
+    { b: 2 }
+  )
+  a.deepEqual(result, {
+    a: 1,
+    b: 2,
+    c: {
+      d: 'd'
+    }
+  })
+})
+
+runner.test('one level update', function () {
+  const result = deepMerge(
+    { a: 1, c: { d: 'd' } },
+    { a: 2, c: { d: 'e' } },
+  )
+  a.deepEqual(result, {
+    a: 2,
+    c: {
+      d: 'e'
+    }
   })
 })
